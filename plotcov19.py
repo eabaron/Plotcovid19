e889f88ca54ae58e32e66bd622266e079ec5d0e6
E Baron <baron@ou.edu>
2021-01-03 16:01:48 -0600

    fixed dates to go into 2021

--- plotcov19.py
+++ plotcov19.py
@@ -3,7 +3,7 @@
 ## Filename:      plotcov19.py
 ## Author:        Eddie Baron <baron@ou.edu>
 ## Created at:    Fri May 22 09:30:20 2020
-## Modified at:   Sun Jul 19 09:35:32 2020
+## Modified at:   Sun Jan  3 16:00:37 2021
 ## Modified by:   Eddie Baron <baron@ou.edu>
 ## Description:   
 ######################################################################
@@ -46,7 +46,42 @@ def bold_labels(ax,fontsize=None):
     tick.label1.set_fontsize(fontsize)
     tick.label1.set_fontweight('bold')
 
-def datestdtojd (stddate):
+import datetime
+import math
+
+def get_julian_datetime(date):
+  """
+    Convert a datetime object into julian float.
+    Args:
+        date: datetime-object of date in question
+  
+    Returns: float - Julian calculated datetime.
+    Raises: 
+        TypeError : Incorrect parameter type
+        ValueError: Date out of range of equation
+    """
+  fmt='%m/%d/%y'
+  try:
+    stddate = datetime.datetime.strptime(date, fmt)
+  except:
+    return
+
+  # Ensure correct format
+  if not isinstance(stddate, datetime.datetime):
+    raise TypeError('Invalid type for parameter "date" - expecting datetime')
+  elif stddate.year < 1801 or stddate.year > 2099:
+    raise ValueError('Datetime must be between year 1801 and 2099')
+
+  # Perform the calculation
+  julian_datetime = 367 * stddate.year - int((7 * (stddate.year + int((stddate.month + 9) / 12.0))) / 4.0) + int(
+    (275 * stddate.month) / 9.0) + stddate.day + 1721013.5 + (
+      stddate.hour + stddate.minute / 60.0 + stddate.second / math.pow(60,
+                                                              2)) / 24.0 - 0.5 * math.copysign(
+                                                                1, 100 * stddate.year + stddate.month - 190002.5) + 0.5
+
+  return julian_datetime - 2400000.5
+
+  def datestdtojd (stddate):
     fmt='%m/%d/%y'
     try:
       sdtdate = datetime.datetime.strptime(stddate, fmt)
@@ -81,7 +116,8 @@ def explore_cases(mystate,df):
   jd = []
   for _ in dates.values:
     if '/' in _:
-      jd.append(datestdtojd(_))
+      # jd.append(datestdtojd(_))
+      jd.append(get_julian_datetime(_)-58930)
   jd = np.asarray(jd)
   
 
@@ -97,12 +133,13 @@ def explore_cases(mystate,df):
     # ymax = 150
     # if mystate == 'OK':
     #   ax.set_ylim([0,ymax])
-    ax.set_xlim([70,jd.max()//10*10+10])
+    # ax.set_xlim([70,jd.max()//10*10+10])
+    ax.set_xlim([0,jd.max()+10])
     md = pd.Series(index=dates,data=diffs)
     run = md.rolling(ns).mean()
     myleg_ = str(ns) +  " day running average"
     ax.plot(jd,run.values,'r',label=myleg_)
-    ax.set_xlabel('Day of Year',fontsize=18)
+    ax.set_xlabel('MJD - 58930',fontsize=18)
     ax.set_ylabel('New Cases/day',fontsize=18)
     ax.legend()
     bold_labels(ax)
@@ -150,7 +187,8 @@ def make_postage(df):
     if i == 1:
       jd = list()
       for _ in dates.values:
-        jd.append(datestdtojd(_))
+        jd.append(get_julian_datetime(_)-58930)
+        # jd.append(datestdtojd(_))
       jd = np.asarray(jd)
   
 
@@ -180,7 +218,8 @@ def make_postage(df):
     #   h.set_loc='best'
     # ax.legend(handles, labels)
     # bold_labels(ax)
-    ax.set_xlim([70,jd.max()//10*10+10])
+    # ax.set_xlim([70,jd.max()//10*10+10])
+    ax.set_xlim([0,jd.max()+10])
     ax.tick_params(axis='x',labelsize=6)
     xtl = ax.get_xticklabels()
     ytl = ax.get_yticklabels()
@@ -192,7 +231,7 @@ def make_postage(df):
         ytl_.set_visible(False)
     ytl[-1].set_visible(True)
 
-  fig.text(0.5, 0.04, 'Day of 2020', ha='center')
+  fig.text(0.5, 0.04, 'MJD-58930', ha='center')
   fig.text(0.8, 0.01, 'Color indicates current y-value', ha='center')
   fig.text(0.04, 0.5, 'New Cases/day', va='center', rotation='vertical')
   m = pylab.cm.ScalarMappable(cmap=pylab.cm.jet)
